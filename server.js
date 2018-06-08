28e4a9e56292c6909c25e63eeb98f8aae0a87fda
Preston Bayle <prestonbayle@gmail.com>
2018-06-07 19:59:23 -0600

    deleted node modules, gitignore

--- server.js
+++ server.js
@@ -10,68 +10,68 @@ require('dotenv').config();
 const app = express();
 const port = 3005;
 
-massive(process.env.CONNECTION_STRING).then(db => {
-    app.set("db", db);
-});
+// massive(process.env.CONNECTION_STRING).then(db => {
+//     app.set("db", db);
+// });
 
 app.use(bodyParser.json());
 
-app.use(
-    session({
-        secret: process.env.SESSION_SECRET,
-        saveUninitialized: true,
-        resave: false
-    })
-)
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(
+//     session({
+//         secret: process.env.SESSION_SECRET,
+//         saveUninitialized: true,
+//         resave: false
+//     })
+// )
+// app.use(passport.initialize());
+// app.use(passport.session());
 
-passport.use(
-    new Auth0Strategy(
-        {
-            domain: process.env.AUTH_DOMAIN,
-            clientID: process.env.AUTH_CLIENT_ID,
-            clientSecret: process.env.AUTH_CLIENT_SECRET,
-            callbackURL: process.env.AUTH_CALLBACK,
-            scope: 'openid profile email'
-        },
-        (accessToken, refreshToken, extraParams, profile, done) => {
-            const db = app.get('db');
-            db.get_user_by_auth_id({ auth_id: profile.id }).then(results => {
-                let user = results[0];
+// passport.use(
+//     new Auth0Strategy(
+//         {
+//             domain: process.env.AUTH_DOMAIN,
+//             clientID: process.env.AUTH_CLIENT_ID,
+//             clientSecret: process.env.AUTH_CLIENT_SECRET,
+//             callbackURL: process.env.AUTH_CALLBACK,
+//             scope: 'openid profile email'
+//         },
+//         (accessToken, refreshToken, extraParams, profile, done) => {
+//             const db = app.get('db');
+//             db.get_user_by_auth_id({ auth_id: profile.id }).then(results => {
+//                 let user = results[0];
 
 
-                if (user) {
-                    return done(null, user);
-                } else {
-                    let userObj = {
-                        name: profile.displayName,
-                        auth_id: profile.id,
-                        picture: profile.picture,
-                        email: profile.emails
-                    };
-                    db.create_user(userObj).then(results => {
-                        let user = results[0];
-                        return done(null, user);
-                    });
-                }
+//                 if (user) {
+//                     return done(null, user);
+//                 } else {
+//                     let userObj = {
+//                         name: profile.displayName,
+//                         auth_id: profile.id,
+//                         picture: profile.picture,
+//                         email: profile.emails
+//                     };
+//                     db.create_user(userObj).then(results => {
+//                         let user = results[0];
+//                         return done(null, user);
+//                     });
+//                 }
 
-            });
-        }
-    )
-);
+//             });
+//         }
+//     )
+// );
 
-passport.serializeUser((user,done) => {
-    return done(null, user.id);
-});
+// passport.serializeUser((user,done) => {
+//     return done(null, user.id);
+// });
 
-passport.deserializeUser((id, done) => {
-    const db = app.get('db');
-        db.get_user_by_id({id}).then(results => {
-        let user = results[0];
-        return done(null,user);
-    });
-});
+// passport.deserializeUser((id, done) => {
+//     const db = app.get('db');
+//         db.get_user_by_id({id}).then(results => {
+//         let user = results[0];
+//         return done(null,user);
+//     });
+// });
 
 app.listen(port, () => {
     console.log('Listening on port ', port);
