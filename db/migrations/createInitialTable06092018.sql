563c0a20a3b0adb77d024803ffefe23fd49b3304
Preston Bayle <prestonbayle@gmail.com>
2018-06-09 10:32:12 -0600

    db file added with migrations folder

---
+++ db/migrations/createInitialTable06092018.sql
@@ -0,0 +1,31 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR,
+    password VARCHAR,
+    admin BOOLEAN,
+    auth_id VARCHAR
+);
+
+CREATE TABLE problems (
+    id SERIAL PRIMARY KEY,
+    difficulty VARCHAR,
+    company VARCHAR,
+    problem_file TEXT,
+    unit_test_file TEXT
+);
+
+CREATE TABLE users_problems_solved (
+    id SERIAL PRIMARY KEY,
+    problem_id INTEGER REFERENCES problems(id),
+    user_id INTEGER REFERENCES users(id),
+    time TIME,
+    points INTEGER 
+);
+
+CREATE TABLE submissions (
+    id SERIAL PRIMARY KEY,
+    difficulty VARCHAR,
+    company VARCHAR,
+    problem_file TEXT,
+    unit_test_file TEXT
+);
