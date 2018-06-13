ca6bd7d51d3241085140558f419a5310cb7dd291
Jake Higgs <jakehiggs@Jakes-MacBook-Pro.local>
2018-06-13 16:57:41 -0600

    will write temp js files to test

---
+++ Controllers/FSController.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+
+module.exports={
+    write: (req, res, next) => {
+        const {content} = req.body
+            fs.writeFile('TempFiles/testFile.js', content, (err) => {
+                if (err) throw err;
+                
+                console.log("The file was succesfully saved!", content);
+                return res.status(200)
+            })
+    },
+    // delete: (req, res, next) => {
+    //         fs.writeFile('TempFiles/testFile.js', content, (err) => {
+    //             if (err) throw err;
+                
+    //             console.log("The file was succesfully saved!", content);
+    //             return res.status(200)
+    //         })
+    // },
+}
