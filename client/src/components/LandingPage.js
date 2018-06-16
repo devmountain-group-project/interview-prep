804e0307705c20bcf70ebca176b2e2e5c3215d73
Chris Gagnier <mcgagnier@yahoo.com>
2018-06-16 10:40:20 -0600

    submission css

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -8,9 +8,9 @@ class LandingPage extends Component {
 
                 <div>
                     <h1 className="text header">Welcome to Interview Prepper</h1>
-                    <p className ="text copy">Let's make some happy little clouds in our world. Trees live in your fan brush, but you have to scare them out. Of course he's a happy little stone, cause we don't have any other kind. Let's put some happy little clouds in our world. Trees cover up a multitude of sins. From all of us here, I want to wish you happy painting and God bless, my friends.</p>
+                    <p className ="text copy">Busey ipsum dolor sit amet. Sometimes horses cough and fart at the same time, so stay out of the range of its butt muscle because a horses butt muscle is thick.Have you urinated? Have you drained your bladder? Are you free? Because if you haven't it will only come out later. I'm giving you some information that your bodily fluids may penetrate your clothing fibre's without warning</p>
 
-                    <p className ="text copy">The very fact that you're aware of suffering is enough reason to be overjoyed that you're alive and can experience it. No pressure. Just relax and watch it happen. Isn't that fantastic? This is your world, whatever makes you happy you can put in it. Go crazy.</p>
+                    <p className ="text copy">These kind of things only happen for the first time once.It's good to yell at people and tell people that you're from Tennesee, so that way you'll be safe.Go with the feeling of the nature. Take it easy. Know why you're here. And remember to balance your internal energy with the environment. </p>
                 </div>
                 <div className="button-contain">
                     <a href="http://localhost:3005/auth"><button className="button1">Login</button></a>
